104b9b8d9442937c6c1e9c67501e4085c8c0cc6c
DC John <dev@dc-john.com>
2026-04-09 20:50:41 +0530

    Fix base path and correctly copy static assets

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   output: 'export',
-  // basePath: '',
+  basePath: '/DrJohn',
   images: {
     unoptimized: true,
     remotePatterns: [

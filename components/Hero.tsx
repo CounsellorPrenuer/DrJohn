bcb587f18a7a11fe2611649693c1e91cd6461c88
DC John <dev@dc-john.com>
2026-07-27 19:58:40 +0530

    fix: make Hero a client component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
 import { urlFor } from '@/lib/sanity'
@@ -347,4 +348,5 @@ function createSpanSegment(originalSpan: any, text: string, makeBlue: boolean, s
     marks,
   }
 }
+
 
